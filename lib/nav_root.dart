4cde7c55fcb0d6d7a2e0c0c2a5c091a945f00841
Malaz Tamim <mft07@mail.aub.edu>
2022-11-19 17:35:56 +0100

    Some edits

--- lib/nav_root.dart
+++ lib/nav_root.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:homeowner_hub/HomeWidget.dart';
+import 'package:homeowner_hub/Services.dart';
 import 'package:homeowner_hub/main.dart';
+import 'package:homeowner_hub/Services.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
 
 
@@ -86,9 +88,7 @@ class NavRootPage extends StatelessWidget {
       MyHomePage(
         title: 'second',
       ),
-      MyHomePage(
-        title: 'thirs',
-      ),
+      Services(),
       MyHomePage(
         title: 'forth',
       ),
